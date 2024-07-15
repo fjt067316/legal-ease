bb583fa9fcca344aab31012c609df2212dbcfee0
Josh <joshuatfolkins@gmail.com>
2024-07-14 22:02:59 -0400

    retrieval doing better

--- model/qa_chain/test/test_retrieval.py
+++ model/qa_chain/test/test_retrieval.py
@@ -3,19 +3,21 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # weird import issues
 import query_answer
 # https://stackoverflow.com/questions/76958817/streamlit-your-system-has-an-unsupported-version-of-sqlite3-chroma-requires-sq
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+# __import__('pysqlite3')
+# import sys
+# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import chromadb # this import must be after the magic stuff before
+
 q = ["when can a landlord evict me?"]
 a = ["A landlord may give a tenant notice of termination of their tenancy on any of the following grounds:", "A landlord may give a tenant notice of termination of the tenancy if the tenant, another occupant of the rental unit or a person whom the tenant permits in the residential complex"]
 
 queries = [
     # "what is a landlord allowed to put in a lease agreement?\n",
     # "what are the contents of a lease agreement\n",
-    "am I allowed pet animals ?",
-    "how can I end my lease early?"
+    "am I allowed a pet?",
+    "when can a landlord enter my unit?"
+    # "how can I end my lease early?",
 ]
 
 def score_query(query, citations):
@@ -31,9 +33,8 @@ if __name__ == "__main__":
     for query in queries:
         
         citations, distances = query_answer.retrieve_citations(query, chroma_client)
-        print(f"{query} ---- {citations}")
         scores = query_answer.get_ranks(query, citations)
-        print(scores)
+        # print(citations)
 
         print(f"{query}\n")
         for score, citation, distance in zip(scores, citations, distances):
